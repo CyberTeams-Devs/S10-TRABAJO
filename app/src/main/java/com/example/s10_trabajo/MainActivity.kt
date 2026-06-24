018d2e51c5a82f47d7673308e57b9eac0fb92c20
dave-1107 <dmaestudios@gmail.com>
2026-06-23 19:53:20 -0500

    ADD clase producto y listado de actividades

--- app/src/main/java/com/example/s10_trabajo/MainActivity.kt
+++ app/src/main/java/com/example/s10_trabajo/MainActivity.kt
@@ -5,16 +5,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val recyclerTareas = findViewById<RecyclerView>(R.id.recyclerTareas)
+
+        val tareas = listOf(
+            "Comprar pan",
+            "Llamar a mama",
+            "Terminar proyecto",
+            "Estudiar Android",
+            "ir al gimnasio"
+        )
+
+        recyclerTareas.layoutManager = LinearLayoutManager(this)
+        recyclerTareas.adapter = TareaAdapter(tareas)
+
         }
     }
 }
