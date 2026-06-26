8f134b3918391fe57cc055e93b9655bb154a4fd0
jeffersonaroca62 <jeffersonaroca62@gmail.com>
2026-06-25 23:57:37 -0500

    ADD de nuevo kt ProductoAdapter y arreglo de kt Producto

--- app/src/main/java/com/example/s10_trabajo/Producto.kt
+++ app/src/main/java/com/example/s10_trabajo/Producto.kt
@@ -2,7 +2,7 @@ package com.example.s10_trabajo
 
 data class Producto(
     val nombre: String,
-    val imageResId: Int,
+    val imagenResId: Int,
     val categoria: String,
     val precio: Float
 )
